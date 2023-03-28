25cb55dbaae48b5aebec858a1441976bab996e4d
alxrji <97622079+alxrji@users.noreply.github.com>
2023-03-28 14:05:42 +0530

    stock update

--- fuelmanagmnt/admin/fuelupdate.php
+++ fuelmanagmnt/admin/fuelupdate.php
@@ -7,9 +7,20 @@
         $dt= date('Y-m-d');
         $price= $_POST['fuel-price'];
         $stock= $_POST['fuel-stock'];
-
-        $sql1="INSERT INTO `fuel`( `date`, `fuel_type`, `price`, `stock`) VALUES ('$dt','$ft','$price','$stock');";
+        $sql="SELECT * FROM `fuel` WHERE `fuel_type`='$ft' ORDER BY fid DESC LIMIT 1;";
+        $result1=mysqli_query($conn,$sql);
+        $row=$result1->fetch_assoc();
+        $st=$row['stock'];
+        $fstock=$stock + $st;
+        $d=$row['date'];
+        if($d != $dt){
+        $sql1="INSERT INTO `fuel`( `date`, `fuel_type`, `price`, `stock`) VALUES ('$dt','$ft','$price','$fstock');";
         $result=mysqli_query($conn,$sql1);
+        }
+        else{
+            $sql1="UPDATE `fuel` SET `price`='$price',`stock`='$fstock' WHERE `fuel_type`='$ft' AND `date`='$dt';";
+            $result=mysqli_query($conn,$sql1); 
+        }
         if(!$result){
             echo'<script> alert ("error ");</script>';
             echo'<script>window.location.href="fuels.php";</script>';
